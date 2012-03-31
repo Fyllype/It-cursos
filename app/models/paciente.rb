4f268d10e4e55c0e694bbcfe2ad596c4b17b243d
Fyllype <fyllype09@gmail.com>
2012-03-31 16:50:32 -0300

    final

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,2 +1,11 @@
 class Paciente < ActiveRecord::Base
+
+ validates_format_of :nome, :with => /\A[a-zA-Z]+\z/, :message => "Apenas Letras"
+ validates_length_of :nome, :minimum => 3
+ validates_length_of :nome, :maximum => 90
+ validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i	 
+ validates_format_of :telefone, :with => /\0[0-9]\9/ , :message => "Apenas números"
+
+ validates :dt, :with => /\0[0-9]\9/ , :message => "Apenas números"
+
 end
